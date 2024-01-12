9bb9523eb1cfd9ef6f50cb719f59fa6b4cfacc15
pbit <alstjqrhkr2018@gmail.com>
2024-01-12 22:38:24 +0900

    Implement FinalObjectModelList

---
+++ ModelListProperties/main.cpp
@@ -0,0 +1,24 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include "datasource.h"
+#include "personmodel.h"
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+
+
+    qmlRegisterType<Person>("com.blikoon.models",1,0,"Person");
+    qmlRegisterType<PersonModel>("com.blikoon.models",1,0,"PersonModel");
+
+
+    const QUrl url(u"qrc:/ModelListProperties/Main.qml"_qs);
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreationFailed,
+        &app, []() { QCoreApplication::exit(-1); },
+        Qt::QueuedConnection);
+    engine.load(url);
+
+    return app.exec();
+}
